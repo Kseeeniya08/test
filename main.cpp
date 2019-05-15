56f0fa52cac90f919f5fc2692e623a81c3ed96c9
Kseeeniya08 <tkseniya849@mail.ru>
2019-05-15 13:16:49 +0400

    1

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "sdt.h"
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cout <<"vvedite a:\n";
+	cin>> a;
+	cout <<"vvedite b:\n";
+	cin>> b;
+	c=a+b;
+	cout<<"summa:"<<c;
+	return 0;
+}
+
