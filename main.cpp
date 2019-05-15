fceee9e6d17bc662df4aed09ae62d8702e50c7c3
Kseeeniya08 <tkseniya849@mail.ru>
2019-05-15 13:29:02 +0400

    изменено

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 int main()
 {
-	int a, b, c;
+	int a, b, c, gg;
 	cout <<"vvedite a:\n";
 	cin>> a;
 	cout <<"vvedite b:\n";
 	cin>> b;
 	c=a-b;
+	t=a+b;
 	cout<<"summa:"<<c;
 	return 0;
 }
